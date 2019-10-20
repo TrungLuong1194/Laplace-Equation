a162ac2ff3dc346fa97f9aec5c8730c305b7902c
TrungLuong1194 <trungluong1194@gmail.com>
2019-10-21 02:30:46 +0300

    21/10

--- Function.h
+++ Function.h
@@ -5,5 +5,6 @@
 using namespace std;
 
 double funtionF(double x, double y) {
-	return (x - 1) * (x - 1) + (y - 1) * (y - 1);
+	//return (x - 1) * (x - 1) + (y - 1) * (y - 1);
+	return x*(x-1)*y*(y-1);
 }
